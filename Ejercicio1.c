be809b84dec5e5aa25ff58c2ff3a5721acc466e3
damariel <88722470+damariel@users.noreply.github.com>
2021-08-18 13:19:46 -0300

    Add files via upload

---
+++ Ejercicio1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	char Calificación;
+	printf("Introduzca una nota");
+	Calificación=getchar();
+	
+	switch(Calificación)
+	{
+		case 'A'
+		puts("La nota es Excelente");
+		break;
+		case 'B'
+	puts("La nota es buena");
+		break;
+	case 'C'
+	puts("La nota es suficiente");
+		break;
+	case 'D'
+	puts("La nota es regular");
+		break;
+	case 'f'
+	puts("La nota es insuficiente");
+		break;
+	default:
+		puts("No se ingreso correctamente");
+	}
+	system("pause");
+}
